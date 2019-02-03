94e3859f321c9172cd989b19624f3ba8e20a7f74
bizurkur <bizurkur@gmail.com>
2019-02-02 20:19:37 -0600

    Changed protected to private

--- tests/AbstractViewTest.php
+++ tests/AbstractViewTest.php
@@ -13,7 +13,7 @@ class AbstractViewTest extends TestCase
     /**
      * @var AbstractView|MockObject
      */
-    protected $fixture = null;
+    private $fixture = null;
 
     protected function setUp(): void
     {
